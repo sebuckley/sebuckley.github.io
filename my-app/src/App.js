c9af332a94d0ecad7c5daa363d2140c82420a995
Stephen <67360857+sebuckley@users.noreply.github.com>
2023-01-22 19:56:52 +0000

    added project and platinum project folders

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,4 +1,4 @@
-import React, { Component }  from 'react';
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
@@ -6,18 +6,13 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+
+        <p className="para">
+          Stephen Buckley Profile
+          <br/>
+          Coming soon ...
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       
       </header>
     </div>
   );
